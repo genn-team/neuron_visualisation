77dc95209b530a288ae809b9ef0f3ea9789b2e52
asakhnenko <asakhnenko@uos.de>
2018-05-18 03:59:21 +0200

    Cleaned up version of a prototype

--- addon-structure.py
+++ addon-structure.py
@@ -1,6 +1,18 @@
 import bpy, bmesh, os, sys
+
 import neuroml
 import neuroml.loaders as loaders
+
+from network.Cell import Cell
+
+cell_dict = {}
+
+def load_cells(includes):
+    for include in includes:
+        cell = loaders.NeuroMLLoader.load('examples/' + include.href)
+        for c in cell.cells:
+            tmp = Cell(c)
+            cell_dict[tmp.id] = tmp
 
 if __name__ == '__main__':
     # --- Clean the scene ---
@@ -12,36 +24,15 @@ if __name__ == '__main__':
     if path[-8:] != '.net.nml':
         sys.exit("Please provide network file")
     network_file = loaders.NeuroMLLoader.load(sys.argv[-1])
+    load_cells(network_file.includes)
 
     for population in network_file.networks[0].populations:
+        cell = cell_dict[population.component]
         for instance in population.instances:
-            print("New instance is being processed")
             x = instance.location.x / 100.0
             y = instance.location.y / 100.0
             z = instance.location.z / 100.0
-            cell = loaders.NeuroMLLoader.load('cADpyr229_L23_PC_5ecbf9b163_0_0.cell.nml')
-            cell_dict = {}
-            for segment in cell.cells[0].morphology.segments:
-                cell_dict[segment.id] = (segment.distal.x / 100.0 + x, segment.distal.y / 100.0 + y, segment.distal.z / 100.0 + z)
-                print("New segment is being processed")
-                if segment.parent == None or segment.distal.diameter > 2.0:
-                    d = segment.distal.diameter / 100.0
-                    bpy.ops.mesh.primitive_uv_sphere_add(segments=64, ring_count=32, size=d,location=cell_dict[segment.id])
-                else:
-                    bpy.ops.object.editmode_toggle()
-                    obj = bpy.context.object
-                    me = obj.data
-                    bm = bmesh.from_edit_mesh(me)
-
-                    v1 = bm.verts.new(cell_dict[segment.id])
-                    v2 = bm.verts.new(cell_dict[segment.parent.segments])
-
-                    bm.edges.new((v1, v2))
-
-                    bmesh.update_edit_mesh(obj.data)
-                    bpy.ops.object.editmode_toggle()
-
-    #cell = loaders.NeuroMLLoader.load(network_file.includes[0].href)
+            cell.update_location((x,y,z))
 
     # --- Render a png ---
     #bpy.data.scenes['Scene'].render.filepath ='./text.png'
