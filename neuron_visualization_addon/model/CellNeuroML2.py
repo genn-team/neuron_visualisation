882c59a6889f6fa1316d2979e5f6cb1c8e3d2fd8
asakhnenko <asakhnenko@uos.de>
2018-06-25 22:22:08 +0200

    Heavy version (opti required) of a cell parser

--- neuron_visualization_addon/model/CellNeuroML2.py
+++ neuron_visualization_addon/model/CellNeuroML2.py
@@ -24,11 +24,10 @@ class CellNeuroML2(Cell):
         # Dictionary of segment ids and locations
         # TODO fix measuremt units
         cell_dict = {}
-        axon = []
         for segment in cell.morphology.segments:
-            if(len(cell_dict) > 150):
+            if(len(cell_dict) > 1500):
                 break
-            cell_dict[segment.id] = (segment.distal.x / 10.0, segment.distal.y / 10.0, segment.distal.z / 10.0)
+            cell_dict[segment.id] = mathutils.Vector((segment.distal.x, segment.distal.y, segment.distal.z)) / 10.0
             if segment.parent == None:
                 # Parent object (soma)
                 d = segment.distal.diameter / 10.0
@@ -37,13 +36,25 @@ class CellNeuroML2(Cell):
                 bpy.context.object.name = self.id
                 # Save referrence
                 self.blender_obj = bpy.context.object
-            elif segment.distal.diameter > 2.0:
-                d = segment.distal.diameter / 10.0
-                bpy.ops.mesh.primitive_uv_sphere_add(segments=64, ring_count=32, size=d, location=cell_dict[segment.id])
-                bpy.context.object.parent = bpy.data.objects[self.id]
-            elif segment.parent.segments + 1 == segment.id:
-                #print(type(self.getLocation()))
-                axon.append(mathutils.Vector(cell_dict[segment.id])) #- self.getLocation())
+            elif segment.proximal == None:
+                # Create curve, curve object and set Cell as a parent
+                cu = bpy.data.curves.new('AxonCurve', 'CURVE')
+                ob = bpy.data.objects.new(str(segment.id), cu)
+                bpy.context.scene.objects.link(ob)
+                ob.parent = self.blender_obj
+
+                # Create bevel object
+                bpy.ops.curve.primitive_bezier_circle_add(radius=segment.distal.diameter/ 10.0)
+                cu.bevel_object = bpy.context.object
+
+                # Create spline and set Bezier control points
+                spline_axon = cu.splines.new('BEZIER')
+                spline_axon.bezier_points[0].co = spline_axon.bezier_points[0].handle_left = spline_axon.bezier_points[0].handle_right = cell_dict[segment.parent.segments]
+                axon = [(cell_dict[segment.id])]
+                spline_axon.bezier_points.add(len(axon))
+                for n in range(len(axon)):
+                    bpt = spline_axon.bezier_points[n+1]
+                    bpt.co = bpt.handle_left = bpt.handle_right = axon[n]
             else:
                 # Create curve, curve object and set Cell as a parent
                 cu = bpy.data.curves.new('AxonCurve', 'CURVE')
@@ -57,9 +68,10 @@ class CellNeuroML2(Cell):
 
                 # Create spline and set Bezier control points
                 spline_axon = cu.splines.new('BEZIER')
+                spline_axon.bezier_points[0].co = spline_axon.bezier_points[0].handle_left = spline_axon.bezier_points[0].handle_right = cell_dict[segment.id]
+                axon = [(segment.proximal.x / 10.0, segment.proximal.y / 10.0, segment.proximal.z / 10.0) ]
                 spline_axon.bezier_points.add(len(axon))
                 for n in range(len(axon)):
                     bpt = spline_axon.bezier_points[n+1]
                     bpt.co = bpt.handle_left = bpt.handle_right = axon[n]
-                axon = [cell_dict[segment.id]]
         #Cell.generated_models[self.id] = self.blender_obj
